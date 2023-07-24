c0b5b43184bdb55e7847eaab993341b5e4a5f743
GCRA87 <giorgio.albieri@burohappold.com>
2023-07-24 16:42:31 +0100

    Minor changes

--- ETABS Base Reactions Exchange/model/PointObj.vb
+++ ETABS Base Reactions Exchange/model/PointObj.vb
@@ -1,28 +1,3 @@
 ﻿Public Class PointObj
 
-    'ATTRIBUTES
-    Private Property x As Double
-    Private Property y As Double
-    Private Property z As Double
-
-
-    'COSTRUCTOR
-    'Default Constructor applies
-
-
-    'GETTERS and SETTERS
-
-    'Setters
-    Public Sub setX(x As Double)
-        Me.x = x
-    End Sub
-    Public Sub setY(y As Double)
-        Me.y = y
-    End Sub
-    Public Sub setZ(z As Double)
-        Me.z = z
-    End Sub
-
-    'Getters
-
 End Class
