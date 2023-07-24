00f73fc207a6772bbba287d33fd005a86a33801e
GCRA87 <giorgio.albieri@burohappold.com>
2023-07-24 18:12:48 +0100

    Add Inheritance to the ETABSConnector Class
    Turn Class ETABSConnector into Abstract.
    Create new concrete class ETABSTrasnferrer inheriting from it
    and allowing to transfer data between ETABS Models.

--- ETABS Base Reactions Exchange/model/ETABSConnector.vb
+++ ETABS Base Reactions Exchange/model/ETABSConnector.vb
@@ -15,49 +15,29 @@ Imports System.ComponentModel
 
 
 
-Public Class ETABSConnector
+Public MustInherit Class ETABSConnector
+    Implements ETABSConnection
 
     'ATTRIBUTES ***********************************************************************************'
-
-    'Private Static/Shared variable for the Singleton Pattern 
-    Private Shared instance As ETABSConnector
 
     'ETABS OAPI Interoperability Variables
     Private helperObject As ETABSv1.cHelper   ' Helper Class Object Variable                  'O(1)
     Private ETABSApp As ETABSv1.cOAPI         ' ETABS Application Object Variable             'O(1)
-    Private sourceEtabsModel As ETABSv1.cSapModel  ' ETABS Model Object Variable              'O(1)
-    Private targetEtabsModel As ETABSv1.cSapModel  ' Target ETABS Model Object Variable       'O(1)
-
-    'Source and Target FileNames
-    Private sourceFileName, targetFileName As String                                          'O(1)
 
     'Utility Variables
-    Private ret As Integer
+    Private ret As Integer                                                                    'O(1)
     Const etabsVisibility As Boolean = False                                                  'O(1)
 
 
-
-    'CONSTRUCTOR - PRIVATE' 'SINGLETON PATTERN ****************************************************'
-    Private Sub New()
+    'CONSTRUCTOR **********************************************************************************'
+    Public Sub New()
     End Sub
 
 
     'METHODS **************************************************************************************'
 
-    'GETINSTANCE() METHOD - SINGLETON PATTERN'
-    Public Shared Function getInstance() As ETABSConnector
-        If instance IsNot Nothing Then
-            Return instance
-        Else
-            instance = New ETABSConnector()
-            Return instance
-        End If
-    End Function
-
-
     'INITIALIZEETABS() METHOD
     Public Sub initialize()
-
         'Helper Class Object Variable
         helperObject = New ETABSv1.Helper                                                                            'O(1)
         'ETABS Application Object Variable                                                                           'O(1)
@@ -65,43 +45,16 @@ Public Class ETABSConnector
         ETABSApp = helperObject.CreateObjectProgID("CSI.ETABS.API.ETABSObject")                                      'O(1)
         'ETABSApp = helperObject.CreateObject("c:\Program Files\Computers and Structures\ETABS 20\ETABS.exe")        'O(1)
         'ETABSApp = helperObject.GetObject("CSI.ETABS.API.ETABSObject")                                              'O(1)
-
     End Sub
 
 
     ' DISPOSEETABS() METHOD
     Public Sub dispose()
-
-        'Save the Model
-        targetEtabsModel.File.Save(setNewFilePath(targetFileName))
-
         'Close the ETABS Application
-        ETABSApp.ApplicationExit(False)
-
+        ETABSApp.ApplicationExit(False) 'O(1)
         'Release Memory
-        sourceEtabsModel = Nothing    'O(1)
-        targetEtabsModel = Nothing    'O(1)
-        ETABSApp = Nothing            'O(1)
-
+        ETABSApp = Nothing              'O(1)
     End Sub
-
-
-    ' SETNEWFILEPATH() METHOD
-    Private Function setNewFilePath(filePath As String) As String
-        Dim newFilePath As String
-        Dim dateObj As Date = Date.Today
-
-        Dim sep() As Char = {"/", "\", "//"}
-
-        With dateObj
-            newFilePath = filePath.Remove(filePath.IndexOf(filePath.Split(sep).Last())) + "BRE" +
-                            .Year.ToString + .Month.ToString("D2") +
-                            .Day.ToString("D2") + "_" + filePath.Split(sep).Last()
-        End With
-
-        Return newFilePath
-
-    End Function
 
 
     ' SETETABSVISIBILITY() METHOD
@@ -112,8 +65,6 @@ Public Class ETABSConnector
             ret = ETABSApp.Unhide()
         End If
     End Sub
-
-
 
 
 End Class
---
+++ ETABS Base Reactions Exchange/model/ETABSTransferrer.vb
@@ -0,0 +1,15 @@
+﻿Public Class ETABSTransferrer
+    Inherits ETABSConnector
+
+    'ATTRIBUTES ***********************************************************************************'
+
+    'ETABS Models
+    Private sourceEtabsModel As ETABSv1.cSapModel  ' ETABS Model Object Variable              'O(1)
+    Private targetEtabsModel As ETABSv1.cSapModel  ' Target ETABS Model Object Variable       'O(1)
+    'Source and Target FileNames
+    Private sourceFileName, targetFileName As String                                          'O(1)
+
+
+
+
+End Class
