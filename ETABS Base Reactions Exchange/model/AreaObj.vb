bedccb95bf77ba4f56111630710ec8ebd8a54c29
GCRA101 <giorgio.albieri@burohappold.com>
2023-10-17 16:14:12 +0100

    Add classes wrapping area and wall properties

---
+++ ETABS Base Reactions Exchange/model/AreaObj.vb
@@ -0,0 +1,113 @@
+﻿Imports ETABS_Base_Reactions_Exchange.model
+
+Public Class AreaObj
+    Implements IComparable
+
+    'ATTRIBUTES
+    Private name As String
+    Private points As List(Of PointObj)
+    Private areaProperty As areaObjProperty
+
+
+    'CONSTRUCTORS
+    'Default
+    Public Sub New()
+    End Sub
+    'Overloaded1
+    Public Sub New(name As String)
+        Me.name = name
+    End Sub
+    'Overloaded2
+    Public Sub New(name As String, points As List(Of PointObj), areaProperty As areaObjProperty)
+        Me.name = name
+        Me.points = points
+        Me.areaProperty = areaProperty
+    End Sub
+
+
+    'METHODS
+
+    'Setters
+    Public Sub setName(name As String)
+        Me.name = name
+    End Sub
+    Public Sub setPoints(points As List(Of PointObj))
+        Me.points = points
+    End Sub
+    Public Sub setAreaProperty(areaProperty As areaObjProperty)
+        Me.areaProperty = areaProperty
+    End Sub
+
+    'Getters
+    Public Function getName() As String
+        Return Me.name
+    End Function
+    Public Function getPoints() As List(Of PointObj)
+        Return Me.points
+    End Function
+
+    Public Function getAreaProperty() As areaObjProperty
+        Return Me.areaProperty
+    End Function
+
+
+    'HASHCODE
+
+    'Method inherited from the Object superclass and that has to be overwritten in order to generate
+    'ad hoc hashcodes based on the values assigned to the specific attributes of this class.
+    'The hashcode is essential to be able to sort and store instances of this class properly 
+    'in whatever concrete implementation of the abstract data structure Hash Table.
+    Public Overrides Function GetHashCode() As Integer
+        'Determines and returns the Hashcode of the class instance as the integer number given 
+        'by the sum of the hashcodes of the name and the corner points
+        Return Me.name.GetHashCode() + Me.getPoints().GetHashCode()
+    End Function
+
+
+    'COMPARETO
+
+    'Method implemented from the IComparable Functional Interface which unique method CompareTo 
+    'gets called everytime we want to compare an instance of this class with another object.
+    'The method needs to be implemented accordingly with the criteria we want to use to define
+    'which object is greater or smaller than the other based on the values assigned to its 
+    'attributes.
+    Public Function CompareTo(obj As Object) As Integer Implements IComparable.CompareTo
+        '1. Check input Obj Data Type to match the AreaObj Class
+        If Not obj.GetType().Equals(Me.GetType) Then
+            Return Nothing
+        End If
+        '2. Down-Cast the input Object to the AreaObjClass
+        Dim areaObj As AreaObj
+        areaObj = CType(obj, AreaObj)
+        '3. Compare the two instances of the class giving precedence to the name
+        Return Me.getName.CompareTo(areaObj.getName())
+    End Function
+
+
+    'EQUALS
+
+    'Method inherited from the Object superclass and that gets called everytime we check whether 
+    'two instances of this class are equal or not. 
+    'It has to be overwritten based on the values assigned to the attributes of the class instances
+    Public Overrides Function Equals(obj As Object) As Boolean
+
+        '1. Check input Obj Data Type to match the AreaObj Class
+        If Not obj.GetType().Equals(Me.GetType) Then
+            Return False
+        End If
+
+        '2. Down-Cast the input Object to the AreaObjClass
+        Dim areaObj As AreaObj
+        areaObj = CType(obj, AreaObj)
+
+        '3. Check if the name and the list of corner points of the two objects area the same
+        If Me.getName.Equals(areaObj.getName) And Me.getPoints.Equals(areaObj.getPoints) Then
+            Return True
+        End If
+
+        Return False
+
+    End Function
+
+
+End Class
---
+++ ETABS Base Reactions Exchange/model/AreaObjProperty.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Runtime.Remoting.Messaging
+
+Public Class AreaObjProperty
+
+    'ATTRIBUTES
+    Protected name As String
+    Protected color As Integer
+    Protected notes As String
+    Protected guid As String
+
+    'CONSTRUCTORS
+    'Default
+    Public Sub New()
+    End Sub
+    'Overloaded
+    Public Sub New(name As String)
+        Me.name = name
+    End Sub
+
+    'METHODS
+
+    'Setters
+    Protected Sub setName(name As String)
+        Me.name = name
+    End Sub
+    Protected Sub setColor(color As Integer)
+        Me.color = color
+    End Sub
+    Protected Sub setNotes(notes As String)
+        Me.notes = notes
+    End Sub
+    Protected Sub setGuid(guid As String)
+        Me.guid = guid
+    End Sub
+
+    'Getters
+    Protected Function getName() As String
+        Return Me.name
+    End Function
+    Protected Function getColor() As Integer
+        Return Me.color
+    End Function
+    Protected Function getNotes() As String
+        Return Me.notes
+    End Function
+    Protected Function getGuid() As String
+        Return Me.guid
+    End Function
+
+End Class
